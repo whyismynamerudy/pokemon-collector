af0ebf4e41f8d040a17efff4f26f26bde477b73b
Rudy <rudraksh.monga@mail.utoronto.ca>
2022-06-09 23:56:45 +0530

    attempting to fix cors

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ mongoose.connect(config.MONGODB_URL)
         console.log("error", error);
     });
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use(cors());
 app.use(express.json()); //this is what allows backend to recieve json
 app.use(express.static(path.join(__dirname, 'build')));
